2a465d0922a25ec81ad3504e40e971f0c5cc2f58
Alex Samuel <alex@alexsamuel.net>
2016-08-14 23:20:41 -0400

    Very rudimentary CSV reader.

--- test/csv0.js
+++ test/csv0.js
@@ -1,26 +1,36 @@
 io = require('data/io')
+synch = require('synchronize')
 
-lineReader = require('readline').createInterface({
-  // input: require('fs').createReadStream('test/accounts.csv')
-  input: process.stdin
-})
+function readCsv(callback) {
+  lineReader = require('readline').createInterface({
+    // input: require('fs').createReadStream('test/accounts.csv')
+    input: process.stdin
+  })
 
-headers = null
-scanners = []
+  headers = null
+  scanners = []
 
-lineReader.on('line', function (line) {
-  const parts = line.split(',')
-  if (headers === null) headers = parts
-  else {
-    const len = parts.length
-    while (scanners.length < len)
-      scanners.push(new io.ColumnScan())
-    for (let i = 0; i < len; ++i)
-      scanners[i].scan(parts[i])
-  }
-})
+  lineReader.on('line', function (line) {
+    const parts = line.split(',')
+    if (headers === null) headers = parts
+    else {
+      const len = parts.length
+      while (scanners.length < len)
+        scanners.push(new io.ColumnScan())
+      for (let i = 0; i < len; ++i)
+        scanners[i].scan(parts[i])
+    }
+  })
 
-lineReader.on('close', function () { 
+  lineReader.on('close', function () { 
+    callback(scanners)
+  })
+}
+
+readCsv(function (scanners) {
   console.log('done')
+  const len = Math.max(...scanners.map(s => s.length))
+  console.log(len)
   console.log(scanners.map(s => s.type))
 })
+
---
+++ test/csv1.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const _ = require('underscore')
+const data = require('data/data')
+const fs = require('fs')
+const io = require('data/io')
+const readline = require('readline')
+
+function parseLine(line) {
+  return line.split(',').map(s => s.trim())
+}
+
+function open(filename) {
+  return fs.createReadStream(filename)
+}
+
+/* async */ function scan(input) {
+  return new Promise((resolve, reject) => {
+    const lineReader = readline.createInterface({ input: input })
+
+    let headers = null
+    let rows = 0
+    const scanners = []
+
+    lineReader.on('line', function (line) {
+      const parts = parseLine(line)
+      if (headers === null) headers = parts
+      else {
+        const len = parts.length
+        while (scanners.length < len)
+          scanners.push(new io.ColumnScan())
+        for (let i = 0; i < len; ++i)
+          scanners[i].scan(parts[i])
+        ++rows
+      }
+    })
+
+    lineReader.on('close', function () { 
+      const cols = _.zip(headers, scanners).map(
+        ([name, s]) => ({name, type: s.type}))
+      resolve({cols, rows})
+    })
+  })
+}
+
+function readData(input, csv) {
+  return new Promise((resolve, reject) => {
+    const cols = csv.cols.length
+    const lineReader = readline.createInterface({input: input})
+
+    let row = 0
+    lineReader.on('line', function (line) {
+      const parts = parseLine(line)
+      if (row > 0)
+        for (let i = 0; i < cols; ++i)
+          csv.arrs[i][row - 1] = parts[i]
+      ++row
+    })
+
+    lineReader.on('close', function () {
+      resolve(csv)
+    })
+  })
+}
+
+function read(filename) {
+  return scan(open(filename)).then(csv => {
+    csv.arrs = csv.cols.map(c => io.newArray(c.type, csv.rows))
+    return readData(open(filename), csv)
+  }).then(csv => {
+    const labels = new Int32Array(_.range(csv.rows))
+    const cols = _.zip(csv.cols.map(_ => _.name), csv.arrs)
+    return new data.Table(new data.Key(labels), cols)
+  }).catch(e => console.log(e))
+}
+
+read('test/accounts.csv').then(t => console.log(t.toString())).catch(console.log)
+
+
