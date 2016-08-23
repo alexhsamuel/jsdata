682de6230a3ae9a255ec4967e74e270e5ac8f1fd
Alex Samuel <alex@alexsamuel.net>
2016-08-23 20:41:13 +0200

    Sample JSON record reader.

---
+++ test/readjson.js
@@ -0,0 +1,51 @@
+`use strict`
+
+const data = require('data/data')
+const fs = require('fs')
+const readline = require('readline')
+
+function open(filename) {
+  return fs.createReadStream(filename)
+}
+
+function read(input) {
+  return new Promise((resolve, reject) => {
+    const lineReader = readline.createInterface({input: input})
+    const values = []
+    lineReader.on('line', line => values.push(JSON.parse(line)))
+    lineReader.on('close', () => resolve(values))
+  })
+}
+
+/**
+ * Returns a `Map` from record name to an array of values.
+ */
+function transpose(records) {
+  const cols = new Map()
+  // FIXME: Need to deal with missing keys.
+  for (const rec of records)
+    for (const name in rec) {
+      const val = rec[name]
+      const vals = cols.get(name)
+      if (vals === undefined) cols.set(name, [val])
+      else vals.push(val)
+    }
+  return cols
+}
+
+// function toArray(arr) {
+//   // FIXME
+//   if (arr.length === 0) return arr
+//   else if 
+// }
+
+read(open('benchmark/filter-sum.json'))
+  .then(recs => {
+    const cols = transpose(recs)
+    // FIXME: Check.
+    const length = cols.values().next().value.length
+    const table = new data.Table(new data.IndexKey(length), [...cols.entries()])
+    console.log(table.toString())
+  })
+  .catch(console.log)
+
