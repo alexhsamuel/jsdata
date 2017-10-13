55e717ecbccddbbd85c99e044de5a07377d02e5e
Alex Samuel <alex@alexsamuel.net>
2017-10-13 00:28:30 -0400

    Series with multikey.

--- test/test_series.js
+++ test/test_series.js
@@ -16,3 +16,13 @@ test('indexed series', t => {
   t.true(s.get(1) == 'bar')
   t.true(s.get(2) == 'baz')
 })
+
+test('multi series', t => {
+  const s = makeSeries([['bar', 'foo', 'foo'], [2, 1, 2]], [100, 101, 102])
+  t.true(s.length == 3)
+  t.true(s.getAt(0) == 100)
+  t.true(s.getAt(2) == 102)
+  t.true(s.get('bar', 2) == 100)
+  t.true(s.get('foo', 1) == 101)
+  t.true(s.get('foo', 2) == 102)
+})
