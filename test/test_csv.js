0208b38dcc23613acb1838c2da81f4b4a98ed8a0
Alex Samuel <alex@alexsamuel.net>
2017-10-15 16:23:10 -0400

    Another CSV test.

--- test/test_csv.js
+++ test/test_csv.js
@@ -11,3 +11,10 @@ test('accounts', t => {
     t.is(tbl.cols.balance.get(3), 400)
   })
 })
+
+test('prec', t => {
+  return readCsv('test/prec.csv').then(tbl => {
+    console.log('' + tbl)
+    t.is(tbl.length, 6)
+  })
+})
