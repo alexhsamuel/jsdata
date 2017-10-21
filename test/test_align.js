6c469ba2f67097dcd3f7196734dae5369595d19f
Alex Samuel <alex@alexsamuel.net>
2017-10-20 22:21:00 -0400

    More align tests.

--- test/test_align.js
+++ test/test_align.js
@@ -8,3 +8,29 @@ test('leftAlign', t => {
   t.deepEqual(Array.from(an), [2, -1, 4, -1, 6, -1])
 })
 
+test('leftAlign 2 remove', t => {
+  const a0 = [
+    ['a', 'a', 'b', 'b', 'b', 'd', 'f', 'f',],
+    [ 2,   3,   1,   3,   8,   2,   0,   3, ],
+  ]
+  const a1 = [
+    ['a',      'b', 'b', 'b',      'f', 'f',],
+    [ 2,        1,   3,   8,        0,   3, ],
+  ]
+  const an = align.leftAlign(a0, a1)
+  t.deepEqual(Array.from(an), [0, -1, 1, 2, 3, -1, 4, 5])
+})
+
+test('leftAlign 2 add', t => {
+  const a0 = [
+    [     'a', 'a',      'b', 'b', 'b', 'd', 'f', 'f',    ],
+    [      2,   3,        1,   3,   8,   2,   0,   3,     ],
+  ]
+  const a1 = [
+    ['a', 'a', 'a', 'a', 'b', 'b', 'b', 'd', 'f', 'f', 'g',],
+    [ 0,   2,   3,   5,   1,   3,   8,   2,   0,   3,   2, ],
+  ]
+  const an = align.leftAlign(a0, a1)
+  t.deepEqual(Array.from(an), [1, 2, 4, 5, 6, 7, 8, 9])
+})
+
